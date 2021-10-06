a716e577419c9897d5a7cb18de8ebb6fe1172c65
CorbinGraham <corbin.graham2@gmail.com>
2021-10-06 13:04:21 -0700

    Functionalize the fetcher

--- lib/http_fetcher.rb
+++ lib/http_fetcher.rb
@@ -1,11 +1,11 @@
 require 'net/http'
 require 'erb'
 
-# Initial Variables for fetching
-base_URL = "https://secure.runescape.com/m=hiscore_oldschool_ironman/group-ironman/view-group?name="
-group_name = ERB::Util.url_encode("pen boys")
-full_URI_path = "#{base_URL}#{group_name}"
+def fetch_hiscores_page(group_name)
+  # Initial Variables for fetching
+  base_URL = "https://secure.runescape.com/m=hiscore_oldschool_ironman/group-ironman/view-group?name="
+  full_URI_path = ERB::Util.url_encode("#{base_URL}#{group_name}")
 
-uri = URI(full_URI_path)
-results = Net::HTTP.get(uri)
-puts(results)
+  uri = URI(full_URI_path)
+  Net::HTTP.get(uri)
+end
